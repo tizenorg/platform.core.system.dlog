2c4f76829b4ce82ef9d97df86e19a933b463e7d7
Kichan Kwon <k_c.kwon@samsung.com>
2016-02-04 11:37:43 +0900

    No longer to use dlog.h.m4
    Divide existing dlog.h.m4 to below
    
     - dlog.h : public API
    
     - dlog-internal.h : macro
    
    Change-Id: I2f310f02323dbd04244493e09ea02916ce4ab663
    Signed-off-by: Kichan Kwon <k_c.kwon@samsung.com>

--- include/dlog-internal.h
+++ include/dlog-internal.h
@@ -17,12 +17,12 @@
  */
 
 /**
- * @file	dlog.h
+ * @file	dlog-internal.h
  * @version	0.4
  * @brief	This file is the header file of interface of dlog.
  */
-#ifndef	_DLOG_H_
-#define	_DLOG_H_
+#ifndef	_DLOG_INTERNAL_H_
+#define	_DLOG_INTERNAL_H_
 
 #include <tizen_error.h>
 #include <stdarg.h>
@@ -45,52 +45,11 @@ extern "C" {
 #define __MODULE__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #endif
 
-m4_ifdef(`TIZEN_ENGINEER_MODE',
+#ifdef TIZEN_ENGINEER_MODE
+#ifndef DLOG_DEBUG_ENABLE
 #define DLOG_DEBUG_ENABLE
-
-)m4_dnl
-/**
- * @addtogroup CAPI_SYSTEM_DLOG
- * @{
- *
- */
-/**
- * @brief Enumeration for Dlog Error.
- * @since_tizen 2.3
- */
-typedef enum {
-	DLOG_ERROR_NONE = TIZEN_ERROR_NONE, /**< Successful */
-	DLOG_ERROR_INVALID_PARAMETER = TIZEN_ERROR_INVALID_PARAMETER, /**< Invalid parameter */
-	DLOG_ERROR_NOT_PERMITTED = TIZEN_ERROR_NOT_PERMITTED /**< Operation not permitted */
-} dlog_error_e;
-/**
- * @}
- */
-
-/**
- * @addtogroup CAPI_SYSTEM_DLOG
- * @{
- *
- */
-/**
- * @brief log priority values, in ascending priority order.
- * @since_tizen 2.3
- */
-typedef enum {
-	DLOG_UNKNOWN = 0, /**< Keep this always at the start */
-	DLOG_DEFAULT, /**< Default */
-	DLOG_VERBOSE, /**< Verbose */
-	DLOG_DEBUG, /**< Debug */
-	DLOG_INFO, /**< Info */
-	DLOG_WARN, /**< Warning */
-	DLOG_ERROR, /**< Error */
-	DLOG_FATAL, /**< Fatal */
-	DLOG_SILENT, /**< Silent */
-	DLOG_PRIO_MAX	/**< Keep this always at the end. */
-} log_priority;
-/**
- * @}
- */
+#endif
+#endif
 
 /**
  * @internal
@@ -426,79 +385,6 @@ static inline int __dlog_no_print(const char *fmt __attribute__((unused)), ...) 
 #endif
 // ---------------------------------------------------------------------
 
-/**
- * @addtogroup CAPI_SYSTEM_DLOG
- * @{
- */
-/**
- * @brief     Send log with priority and tag.
- * @details   for application
- * @since_tizen 2.3
- * @param[in] prio log_priority
- * @param[in] tag tag
- * @param[in] fmt format string
- * @return On success, the function returns the number of bytes written.
- *         On error, a negative errno-style error code
- * @retval #DLOG_ERROR_INVALID_PARAMETER Invalid parameter
- * @retval #DLOG_ERROR_NOT_PERMITTED Operation not permitted
- * @pre       none
- * @post      none
- * @see       dlog_vprint
- * @code
-#include<dlog.h>
-int main(void)
-{
-    int integer = 21;
-    char string[] = "test dlog";
-
-	dlog_print(DLOG_INFO, "USR_TAG", "test dlog");
-	dlog_print(DLOG_INFO, "USR_TAG", "%s, %d", string, integer);
-    return 0;
-}
- * @endcode
- */
-int dlog_print(log_priority prio, const char *tag, const char *fmt, ...);
-
-/**
- * @brief     Send log with priority, tag and va_list.
- * @details   for application
- * @since_tizen 2.3
- * @param[in] prio log_priority
- * @param[in] tag tag
- * @param[in] fmt format string
- * @param[in] ap va_list
- * @return On success, the function returns the number of bytes written.
- *         On error, a negative errno-style error code
- * @retval #DLOG_ERROR_INVALID_PARAMETER Invalid parameter
- * @retval #DLOG_ERROR_NOT_PERMITTED Operation not permitted
- * @pre       none
- * @post      none
- * @see       dlog_print
- * @code
-#include<dlog.h>
-void my_debug_print(char *format, ...)
-{
-    va_list ap;
-
-    va_start(ap, format);
-    dlog_vprint(DLOG_INFO, "USR_TAG", format, ap);
-    va_end(ap);
-}
-
-int main(void)
-{
-    my_debug_print("%s", "test dlog");
-    my_debug_print("%s, %d", "test dlog", 21);
-    return 0;
-}
- * @endcode
- */
-int dlog_vprint(log_priority prio, const char *tag, const char *fmt, va_list ap);
-/**
- * @}
- */
-
-
 /*
  * The stuff in the rest of this file should not be used directly.
  */
@@ -520,7 +406,7 @@ int dlog_vprint(log_priority prio, const char *tag, const char *fmt, va_list ap)
  * @see       __dlog_print
  * @code
     #define LOG_TAG USR_TAG
-    #include<dlog.h>
+    #include<dlog-internal.h>
      __dlog_print(LOG_ID_MAIN, DLOG_INFO, USR_TAG, "you must not use this API directly");
  * @endcode
  */
@@ -545,7 +431,7 @@ int __dlog_print(log_id_t log_id, int prio, const char *tag, const char *fmt, ..
  * @see       __dlog_vprint
  * @code
     #define LOG_TAG USR_TAG
-    #include<dlog.h>
+    #include<dlog-internal.h>
      __dlog_vprint(LOG_ID_MAIN, DLOG_INFO, USR_TAG, "you must not use this API directly", ap);
  * @endcode
   */
@@ -553,4 +439,4 @@ int __dlog_vprint(log_id_t log_id, int prio, const char *tag, const char *fmt, v
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
-#endif /* _DLOG_H_*/
+#endif /* _DLOG_INTERNAL_H_*/
