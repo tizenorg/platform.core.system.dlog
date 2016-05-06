0c9d8e09ee97a431195e957e62bbfe401c9eef9b
Michal Bloch <m.bloch@samsung.com>
2016-05-06 19:56:23 +0200

    Add a pipe-based userspace logging backend.
    Change-Id: I1dca48f71ecf637c71f651f511ceca8d99ca16d1
    Signed-off-by: Kazimierz Krosman <k.krosman@samsung.com>
    Signed-off-by: Michal Bloch <m.bloch@samsung.com>

--- src/shared/logcommon.c
+++ src/shared/logcommon.c
@@ -18,6 +18,10 @@
 
 #include <string.h>
 
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h>
 #include <logcommon.h>
 
 #define MAX_PREFIX_SIZE 32
@@ -53,4 +57,38 @@ char * log_name_by_id (log_id_t id)
 			return logid_map[i].name;
 
 	return "";
+}
+
+int recv_file_descriptor(int socket)
+{
+	struct msghdr message;
+	struct iovec iov[1];
+	struct cmsghdr *control_message = NULL;
+	char ctrl_buf[CMSG_SPACE(sizeof(int))];
+	char data[1];
+	int res;
+
+	memset(&message, 0, sizeof(struct msghdr));
+	memset(ctrl_buf, 0, CMSG_SPACE(sizeof(int)));
+
+	/* For the dummy data */
+	iov[0].iov_base = data;
+	iov[0].iov_len = sizeof(data);
+
+	message.msg_name = NULL;
+	message.msg_namelen = 0;
+	message.msg_control = ctrl_buf;
+	message.msg_controllen = CMSG_SPACE(sizeof(int));
+	message.msg_iov = iov;
+	message.msg_iovlen = 1;
+
+	if((res = recvmsg(socket, &message, 0)) <= 0)
+		return res;
+
+	/* Iterate through header to find if there is a file descriptor */
+	for(control_message = CMSG_FIRSTHDR(&message); control_message != NULL; control_message = CMSG_NXTHDR(&message, control_message)) {
+		if( (control_message->cmsg_level == SOL_SOCKET) && (control_message->cmsg_type == SCM_RIGHTS) )
+			return *(CMSG_DATA(control_message));
+	}
+	return -1;
 }
